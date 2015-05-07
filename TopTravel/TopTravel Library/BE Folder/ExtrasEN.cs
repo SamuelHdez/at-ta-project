153280b21d2a150b855cfe0091680508602b1db9
SamuelHdez <s.hernandezmartos@gmail.com>
2015-05-07 19:57:42 +0200

    ExtrasCAD and EN

--- TopTravel/TopTravel Library/BE Folder/ExtrasEN.cs
+++ TopTravel/TopTravel Library/BE Folder/ExtrasEN.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,72 @@ namespace TopTravel
 {
     class ExtrasEN
     {
+        public ExtrasEN(int ID=-1, string w="", string f="", int d=-1)
+        {
+            id = ID;
+            wifi = w;
+            food = f;
+            discount = d;
+        }
+
+       public void add_extra()
+       {
+           ExtrasCAD c = new ExtrasCAD();
+           c.addExtra(this);
+       }
+
+       public void delete_extra()
+       {
+           ExtrasCAD c = new ExtrasCAD();
+           c.deleteExtra(this);
+       }
+
+       public void update_extra()
+       {
+           ExtrasCAD c = new ExtrasCAD();
+           c.updateExtra(this);
+       }
+
+
+       public ArrayList showAllHotels()
+       {
+           ArrayList a = new ArrayList();
+           ExtrasCAD c = new ExtrasCAD();
+           a = c.showExtras();
+
+           return a;
+       }
+
+        //propierties
+        private int id;
+        private string wifi;
+        private string food;
+        private int discount;
+
+        //getters and setters
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string WiFi
+        {
+            get { return wifi; }
+            set { wifi = value; }
+        }
+
+        public string Food
+        {
+            get { return food; }
+            set { food = value; }
+        }
+
+        public int Discount
+        {
+            get { return discount; }
+            set { discount = value; }
+        }
     }
 }
--- TopTravel/TopTravel Library/CAD Folder/ExtrasCAD.cs
+++ TopTravel/TopTravel Library/CAD Folder/ExtrasCAD.cs
@@ -1,13 +1,121 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Configuration;
 
 namespace TopTravel
 {
     class ExtrasCAD
     {
-        // To-do make this when all changes are according to database scheme
+        ArrayList lista = new ArrayList();
+        ArrayList search = new ArrayList();
+
+        public void addExtra(ExtrasEN e)
+        {
+            string s;
+            s = ConfigurationManager.ConnectionStrings["DatabaseConnection"].ToString();
+            ExtrasEN ex = e;
+            SqlConnection c = new SqlConnection(s);
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("Insert Into Extras (Id,WiFi,Food,Discount) VALUES ('" + ex.Id + "','" + ex.WiFi + "','" + ex.Food + "','" +
+                    ex.Discount + "')", c);
+
+                com.ExecuteNonQuery();
+            }
+            catch (Exception exc)
+            {
+                exc.ToString();
+                Console.WriteLine("ERROR: Add extra");
+            }
+            finally
+            {
+                c.Close();
+            }
+        }
+
+        public void deleteExtra(ExtrasEN e)
+        {
+            string s;
+            s = ConfigurationManager.ConnectionStrings["DatabaseConnection"].ToString();
+            ExtrasEN ex = e;
+            SqlConnection c = new SqlConnection(s);
+
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("Delete From Extras Where Id = " + ex.Id, c);
+                com.ExecuteNonQuery();
+            }
+            catch (Exception exc)
+            {
+                exc.ToString();
+                Console.WriteLine("ERROR: Delete extra");
+            }
+            finally
+            {
+                c.Close();
+            }
+        }
+
+        public void updateExtra(ExtrasEN e)
+        {
+            string s;
+            s = ConfigurationManager.ConnectionStrings["DatabaseConnection"].ToString();
+            ExtrasEN ex = e;
+            SqlConnection c = new SqlConnection(s);
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("Update Extras Set WiFi = '" + ex.WiFi + "', Food = '" + ex.Food + "', Discount ='" +
+                    ex.Discount + "' Where Id = " + ex.Id, c);
+                com.ExecuteNonQuery();
+            }
+            catch (Exception exc)
+            {
+                exc.ToString();
+                Console.WriteLine("ERROR: Update extra");
+            }
+            finally
+            {
+                c.Close();
+            }
+        }
+
+        public ArrayList showExtras()
+        {
+            string s;
+            s = ConfigurationManager.ConnectionStrings["DatabaseConnection"].ToString();
+            SqlConnection c = new SqlConnection(s);
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("Select * from Extras", c);
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    lista.Add(dr["Id"].ToString());
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                Console.WriteLine("ERROR: Show extras");
+            }
+            finally
+            {
+                c.Close();
+            }
+            return lista;
+        }
+
     }
 }
