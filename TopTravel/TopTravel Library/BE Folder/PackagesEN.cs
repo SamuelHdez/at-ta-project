1fe9fbc411fbca3cfa2c832d6c9dad7670794f38
Lukeblanes <lukeblanes@gmail.com>
2015-05-08 12:02:38 +0200

    Package
    tambien he modificado EN and CAD.txt

--- TopTravel/TopTravel Library/BE Folder/PackagesEN.cs
+++ TopTravel/TopTravel Library/BE Folder/PackagesEN.cs
@@ -8,46 +8,39 @@ namespace TopTravel
     // o PackageTour o Packageholiday o VacationPackage ...
     public class PackagesEN
     {
-        public PackagesEN(int id, string dt, int p, string dp, DateTime d, int nd)
+        public PackagesEN(int id, string n, int t, int h)
         {
-            ID = id;
-            Destination = dt;
-            Price = p;
-            Description = dp;
-            Date = d;
-            NumDays = nd;
+            Id = id;
+            Name = n;
+            Transport = t;
+            Hotel = h;
         }
 
         public void add_pack()
         {
-            m_cc = new PackagesCAD();
-            m_cc.add_pack(this);
+            PackagesCAD c = new PackagesCAD();
+            c.add_pack(this);
         }
 
         public void delete_pack()
         {
-            m_cc = new PackagesCAD();
-            m_cc.add_pack(this);
+            PackagesCAD c = new PackagesCAD();
+            c.delete_pack(this);
         }
 
         // PROPERTIES
-        public int ID { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Transport { get; set; } // Flight
+        public int Hotel { get; set; }
 
-        // pricipales caracteristicas
-        public string Destination { get; set; }
-        public int Price { get; set; }
-        public string Description { get; set; }
-        public DateTime Date { get; set; }
-        public int NumDays { get; set; }
-
+/*
         // claves ajenas a hoteles y transporte que tenga
         public virtual HotelEN Hotel { get; set; } //---------> crear entidad HotelEN
         public virtual FlightEN Flight { get; set; } //-------> crear entidad FlightEN
         public virtual TrainEN Train { get; set; }
         public virtual CruiseEN Cruise { get; set; } //-------> crear entidad FlightEN
         public virtual BusEN Bus { get; set; } //-------------> crear entidad BusEN
-
-        // DATA
-        private PackagesCAD m_cc;
+*/
     }
 }
