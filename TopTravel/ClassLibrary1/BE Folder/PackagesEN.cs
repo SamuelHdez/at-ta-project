a532e2f1189bc49c67f7a401cf53f62999204844
Xuplus <antovillaescusa@gmail.com>
2015-05-20 09:32:28 +0200

    Packages
    update

--- TopTravel/ClassLibrary1/BE Folder/PackagesEN.cs
+++ TopTravel/ClassLibrary1/BE Folder/PackagesEN.cs
@@ -1,12 +1,104 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
 
 namespace TopTravel
 {
     public class PackagesEN
     {
+        public PackagesEN()
+        {
+        }
+
+        public DataSet add_Packages()
+        {
+            PackagesCAD c = new PackagesCAD();
+            DataSet ds = c.addPackages(this);
+            return ds;
+        }
+
+        public DataSet delete_Packages(int i)
+        {
+            PackagesCAD c = new PackagesCAD();
+            DataSet ds = c.deletePackages(this, i);
+            return ds;
+        }
+
+        public DataSet update_Packages(int i)
+        {
+            PackagesCAD c = new PackagesCAD();
+            DataSet ds = c.updatePackages(this, i);
+            return ds;
+        }
+
+        public DataSet searchAllPackages(String city1, String city2)
+        {
+            PackagesCAD c = new PackagesCAD();
+            DataSet ds = c.searchPackages(city1, city2);
+            return ds;
+        }
+
+        public DataSet searchIDPackages(String idF)
+        {
+            PackagesCAD c = new PackagesCAD();
+            DataSet ds = c.searchIDPackages(idF);
+            return ds;
+        }
+
+        public DataSet showAllPackages()
+        {
+            PackagesCAD c = new PackagesCAD();
+            DataSet ds = c.showPackages(this);
+            return ds;
+        }
+
+        // PROPERTIES
+        private int Id;
+        private string name;
+        private int transport; // Flight
+        private int hotel;
+        private string image;
+        private int price;
+
+        public int id
+        {
+            get { return Id;}
+            set { Id = value; }
+        }
+
+        public string Name
+        {
+            get { return name;}
+            set { name = value; }
+        }
+
+        public int Transport
+        {
+            get { return transport;}
+            set { transport = value; }
+        }
+
+        public int Hotel
+        {
+            get { return hotel;}
+            set { hotel = value; }
+        }
+
+        public string Image
+        {
+            get { return image;}
+            set { image = value; }
+        }
+
+        public int Price
+        {
+            get { return price;}
+            set { price = value; }
+        }
         /*
         public PackagesEN(int id, string n, int t, int h)
         {
